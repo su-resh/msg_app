ffca9474b3e96ac68cfe7e8f1fd55d96af6f7624
Suresh Subedi <sureshsubedi3994@gmail.com>
2024-04-27 15:21:14 +0545

    Complete working app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:msg_app/pages/register_page.dart';
 import 'package:msg_app/pages/splash_screen.dart';
 import 'package:msg_app/firebase_options.dart';
 import 'package:msg_app/themes/theme_provider.dart';
